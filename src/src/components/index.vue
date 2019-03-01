9674c1911bf93946ef994e6ee6f9707b878565f9
sunhaoyue <sunhaoyue@baidu.com>
2019-03-01 16:49:39 +0800

    vue

---
+++ src/src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>测试</div>
+</template>
+<script>
+export default {
+    name: 'index',
+    components: {},
+    data() {
+        return {};
+    },
+    props: [],
+    events: {},
+    mounted() {},
+    methods: {}
+};
+</script>
+<style scoped lang='less'></style>
