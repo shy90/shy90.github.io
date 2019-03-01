30514f17521303e3430f88ed91c750f7c0f0214b
sunhaoyue <sunhaoyue@baidu.com>
2019-03-01 16:12:18 +0800

    测试

---
+++ precache-manifest.a9ab1b23ddeaae8bba70b70f38bd0cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f1e7d49aa3f84b021b85",
+    "url": "/static/js/main.f1e7d49a.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "f1e7d49aa3f84b021b85",
+    "url": "/static/css/main.b42bddb6.chunk.css"
+  },
+  {
+    "revision": "244be6c001a396926a19e5ae542fdf04",
+    "url": "/index.html"
+  }
+];
